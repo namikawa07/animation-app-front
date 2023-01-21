3c5c6e045710cdfe912ade7dc741b3bb1ad78b2e
namikawa07 <tatsuki111407@gmail.com>
2023-01-21 13:14:49 +0900

    インプット部分とボタンの追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,91 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { IonPage,  IonModal,
-  IonContent,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonAvatar,
-  IonImg,IonHeader, IonToolbar, IonTitle, IonIcon, IonButton, IonButtons, IonBackButton, IonSegment, IonSegmentButton} from '@ionic/react';
+import { IonPage } from '@ionic/react';
 import './Home.css';
-
 import Header from '../components/Header'
 import VideoContentList from '../components/video/content/List';
+import SignupModal from '../components/SignupModal';
 
 const Home: React.FC = () => {
-
-  const modal = useRef<HTMLIonModalElement>(null);
   const page = useRef(null);
-  const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
-
-  useEffect(() => {
-    setPresentingElement(page.current);
-  }, []);
-  function dismiss() {
-    modal.current?.dismiss();
-  }
 
   return (
     <IonPage ref={page}>
     <Header></Header>
      <VideoContentList></VideoContentList>
-     <IonModal ref={modal} trigger="open-modal" presentingElement={presentingElement!}>
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>ログイン</IonTitle>
-              <IonButtons slot="end">
-                <IonButton color="dark" onClick={() => dismiss()}>閉じる</IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <IonSegment value="signup">
-              <IonSegmentButton value="signup">
-                <IonLabel>新規登録</IonLabel>
-              </IonSegmentButton>
-              <IonSegmentButton value="signin">
-                <IonLabel>ログイン</IonLabel>
-              </IonSegmentButton>
-            </IonSegment>
-            <IonList>
-              <IonItem>
-                <IonAvatar slot="start">
-                  <IonImg src="https://i.pravatar.cc/300?u=b" />
-                </IonAvatar>
-                <IonLabel>
-                  <h2>Connor Smith</h2>
-                  <p>Sales Rep</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonAvatar slot="start">
-                  <IonImg src="https://i.pravatar.cc/300?u=a" />
-                </IonAvatar>
-                <IonLabel>
-                  <h2>Daniel Smith</h2>
-                  <p>Product Designer</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonAvatar slot="start">
-                  <IonImg src="https://i.pravatar.cc/300?u=d" />
-                </IonAvatar>
-                <IonLabel>
-                  <h2>Greg Smith</h2>
-                  <p>Director of Operations</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonAvatar slot="start">
-                  <IonImg src="https://i.pravatar.cc/300?u=e" />
-                </IonAvatar>
-                <IonLabel>
-                  <h2>Zoey Smith</h2>
-                  <p>CEO</p>
-                </IonLabel>
-              </IonItem>
-            </IonList>
-          </IonContent>
-        </IonModal>
+     <SignupModal openPage={page}></SignupModal>
     </IonPage>
   );
 };
