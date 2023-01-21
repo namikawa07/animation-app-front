d08064ce480513d2bac5c84a7898d43678e67be9
namikawa07 <tatsuki111407@gmail.com>
2023-01-21 13:22:43 +0900

    いらないコードの削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,22 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonIcon, IonButton, IonButtons, IonBackButton,
-  IonModal,
-  IonContent,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonAvatar,
-  IonImg, IonPage} from '@ionic/react';
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle, IonIcon, IonButton, IonButtons, IonBackButton } from '@ionic/react';
 import { personCircle } from 'ionicons/icons';
 import { useLocation } from "react-router-dom"
-
-import {locationType} from '../../src/types/global'
+import { locationType } from '../../src/types/global'
 
 interface HeaderProps { }
 
-
-
 const Header: React.FC<HeaderProps> = () => {
-  const modal = useRef<HTMLIonModalElement>(null);
-  const page = useRef(null);
-  const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
-
-  useEffect(() => {
-    setPresentingElement(page.current);
-  }, []);
-  function dismiss() {
-    modal.current?.dismiss();
+  let BackButton  = <></>
+  const location = useLocation<locationType>()
+  if (location.pathname === '/account') {
+    BackButton = <IonButtons slot="start">
+    <IonBackButton text="戻る" defaultHref="/home"></IonBackButton>
+    </IonButtons>
   }
-
-let BackButton  = <></>
-const location = useLocation<locationType>()
-if (location.pathname === '/account') {
-  BackButton = <IonButtons slot="start">
-  <IonBackButton text="戻る" defaultHref="/home"></IonBackButton>
-  </IonButtons>
-}
 
   return (
   <>
-
     <IonHeader translucent>
       <IonToolbar>
         {BackButton}
