d337378476b0bc6c1f925de2d0e5ea72655de9d5
namikawa07 <tatsuki111407@gmail.com>
2023-01-22 22:21:17 +0900

    いらないimportの削除

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { IonPage } from '@ionic/react'
 import './Home.css'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import VideoContentList from '../components/video/content/List'
-import SignupModal from '../components/SignupModal'
 
 const Home: React.FC = () => {
   const page = useRef(null)
